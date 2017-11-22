dfd70a0777ac67393f7bd898f7e41b0a60c7be94
Piotr Karpiewski <karpiewski.git@gmail.com>
2017-11-22 17:31:39 +0100

    Polishing the code

--- xy_recorder.ino
+++ xy_recorder.ino
@@ -3,7 +3,7 @@
 
   Sends data to TDA1543 double DAC
 
- */
+*/
  
 // DAC PINs
 int ws = 2;
@@ -13,40 +13,38 @@ int data = 4;
 int mode = 12;
 
 int ii = 0;
-short val1 = 0xFF;
-short val2 = 0xFF;
+short val1 = 0xFF;  // current value of X
+short val2 = 0xFF;  // current value of Y
 
 // warning: int is -32768..32767 on UNO!
 long target_val1 = 0;
 long target_val2 = 0;
-double dval1 = (double)val1;
-double dval2 = (double)val2;
+double dval1 = (double)val1;  // current value of x in double and not int
+double dval2 = (double)val2;  // current value of Y in double and not int
 
 int caret_speed = 5;
-double dstep1 = caret_speed;
-double dstep2 = caret_speed;
+double dstep1 = caret_speed;  // initial value of step X
+double dstep2 = caret_speed;  // initial value of step Y
 
 char outdbg[300];
 
 
 // the setup routine runs once when you press reset:
 void setup() {                
-  // initialize the digital pin as an output.
+  // Pins for TDA1543 DAC
   pinMode(ws, OUTPUT);     
   pinMode(bck, OUTPUT);     
   pinMode(data, OUTPUT);     
+  randomSeed(1809);
 
-  pinMode(mode, INPUT);
+
+  pinMode(mode, INPUT);  // 0 - work, 1 - test
 
 
   Serial.begin(9600); 
-
-    target_val1 = random(65535);
-    target_val2 = random(65535);
-    
-  randomSeed(1809);
-
 }
+
+// DAC functions
 
 void send_data(short value_l, short value_r) {
   int ff;
@@ -73,19 +71,6 @@ void send_data(short value_l, short value_r) {
 
 void set_target(int trg1, int trg2) {
   
-  //trg1 = trg1 > 32767 ? 32767 : trg1;
-  //trg2 = trg2 > 32767 ? 32767 : trg2;
-/*  
-    Serial.print("val1:");
-    Serial.print(val1);
-    Serial.print("  val2:");
-    Serial.print(val2);
-
-    Serial.print("  trg1:");
-    Serial.print(trg1);
-    Serial.print("  trg2:");
-    Serial.print(trg2);
-*/    
   target_val1 = trg1;
   target_val2 = trg2;
   
@@ -100,15 +85,6 @@ void set_target(int trg1, int trg2) {
     dstep1 = ((double)target_val1 - dval1) / abs((double)target_val2 - dval2) * (double)caret_speed;
     dstep2 = ((double)target_val2 - dval2) / abs((double)target_val2 - dval2) * (double)caret_speed;
   }
-
-/*
-    Serial.print("  dstep1:");
-    Serial.print((long)dstep1);
-    Serial.print("  dstep2:");
-    Serial.print((long)dstep2);
-    Serial.print("  ii:");
-    Serial.println(ii);
-*/
 }
 
 int step_ahead() {
@@ -132,57 +108,71 @@ int step_ahead() {
     return (val1 == target_val1 && val2 == target_val2);  // TRUE = target reached;
 }
 
-// the loop routine runs over and over again forever:
+int next_test_mode_target() {
+    static int arrived = 0;  // 0 - cross, 1 - V or H
+
+    if(abs(target_val1) != 32767)
+        target_val1 = -abs(target_val1)/target_val1 * 32767;
+    if(abs(target_val2) != 32767)
+        target_val1 = -abs(target_val2)/target_val2 * 32767;
+        
+    if(arrived) {
+        // arrived V or H, next go across
+        // now change both X and Y
+        target_val1 = -target_val1;
+        target_val2 = -target_val2;
+        arrived = 0;
+    }
+    else
+    {
+        // arrived cross, next go V or H
+        if(target_val1 == target_val2)
+            // now change just Y
+            target_val2 = -target_val2;
+        else
+            // now change just X
+            target_val1 = -target_val1;
+        arrived = 1;
+    }
+}
+
+// End of DAC functions
+
+
+
+
+
+
+
 void loop() {
   //delay(1);
-  
+
+  // DAC controlling section
+  // send data to DAC
   send_data(val1, val2);
-  
   if(ii >= 5000) {
-    
     ii = 0;
-    // caret_speed = random(5) + 5;
-
     if(digitalRead(mode) == HIGH) {
-      if(ii == 0)
-        target_val1 = (random(2)*2-1) * 32767;
-        target_val2 = (random(2)*2-1) * 32767;
-
-        //target_val1 = (random(3)-1) * 32767;
-        //target_val2 = (random(3)-1) * 32767;
-    } else
-    {
-      // Randomize hard
-      //randomSeed(analogRead(1));
-      target_val1 = random(-32768, 32767);
-      target_val2 = random(-32768, 32767);
-sprintf(outdbg, "seed:%d  val1:%ld  val2:%ld", 1809, target_val1, target_val2);
-Serial.println(outdbg);
+        next_test_mode_target();
+    } else {
+        target_val1 = random(-32768, 32767);
+        target_val2 = random(-32768, 32767);
     }
-
-    // set target and respective steps to have both channels
-    // reach their targets at the same time
     set_target(target_val1, target_val2);
   }
-  
   // if i == 0 do not do anything; just wait for reaching the target
   if (ii > 0) {
     ii++;
   }
- 
- 
   if( step_ahead() ) { // TRUE = target reached
-
-/*
-sprintf(outdbg, "ii:%d  val1:%d  trg1:%ld  dstep1:%ld    val2:%d  trg2:%ld  dstep2:%ld", 
-                ii, 
-                val1, target_val1, (long)(dstep1*100), 
-                val2, target_val2, (long)(dstep2*100));
-Serial.println(outdbg);
-*/
     if(ii == 0)
       ii = 1; // bump it to make it counting
   }
+  // End of DAC controlling section
+  
+  
+  
+  
 
 /*
     Serial.print((long)target_val1);
