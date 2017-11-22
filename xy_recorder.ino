b38451d04c16a9f79187bdcc40470b41ab55e72a
Piotr Karpiewski <karpiewski.git@gmail.com>
2017-11-22 17:50:11 +0100

    Added dial wheel routines

--- xy_recorder.ino
+++ xy_recorder.ino
@@ -2,47 +2,50 @@
   xy_recorder.ino
 
   Sends data to TDA1543 double DAC
+  Added dial controller
 
 */
  
-// DAC PINs
+#define DEBUG_DAC
+#undef DEBUG_DIAL
+#define DEBUG_STORY
+
+// Pin 13 has an LED connected on most Arduino boards.
+int led = 13;
+
+// plotter pins
+int mode = 12;
+int powerOff = 9;  // 0 - plotter ON, 1 - plotter OFF
+
+// DAC pins
 int ws = 2;
 int bck = 3;
 int data = 4;
 
-int mode = 12;
+// dial pins
+int dialInputPin = 7;
+int soundOutputPin = 8;
+int tonePin = 10;
 
-int ii = 0;
+
+// DAC variables
 short val1 = 0xFF;  // current value of X
 short val2 = 0xFF;  // current value of Y
-
 // warning: int is -32768..32767 on UNO!
 long target_val1 = 0;
 long target_val2 = 0;
 double dval1 = (double)val1;  // current value of x in double and not int
 double dval2 = (double)val2;  // current value of Y in double and not int
-
 int caret_speed = 5;
 double dstep1 = caret_speed;  // initial value of step X
 double dstep2 = caret_speed;  // initial value of step Y
 
+int story_length = 0;  // -1 - endless
+
 char outdbg[300];
 
 
-// the setup routine runs once when you press reset:
-void setup() {                
-  // Pins for TDA1543 DAC
-  pinMode(ws, OUTPUT);     
-  pinMode(bck, OUTPUT);     
-  pinMode(data, OUTPUT);     
-  randomSeed(1809);
 
-
-  pinMode(mode, INPUT);  // 0 - work, 1 - test
-
-
-  Serial.begin(9600); 
-}
 
 // DAC functions
 
@@ -69,6 +72,7 @@ void send_data(short value_l, short value_r) {
   }
 }
 
+
 void set_target(int trg1, int trg2) {
   
   target_val1 = trg1;
@@ -81,10 +85,20 @@ void set_target(int trg1, int trg2) {
     dstep1 = ((double)target_val1 - dval1) / abs((double)target_val1 - dval1) * (double)caret_speed;
     dstep2 = ((double)target_val2 - dval2) / abs((double)target_val1 - dval1) * (double)caret_speed;
   }
-  else {
+  else 
+  if(abs((double)target_val2 - dval2) > 0) {
     dstep1 = ((double)target_val1 - dval1) / abs((double)target_val2 - dval2) * (double)caret_speed;
     dstep2 = ((double)target_val2 - dval2) / abs((double)target_val2 - dval2) * (double)caret_speed;
   }
+  else {
+    dstep1 = 0;
+    dstep2 = 0;
+  }
+
+#ifdef DEBUG_DAC
+  sprintf(outdbg, "set target: %lf %d   %lf %d", dval1, target_val1, dval2, target_val2);
+  Serial.println(outdbg);
+#endif
 }
 
 int step_ahead() {
@@ -108,13 +122,24 @@ int step_ahead() {
     return (val1 == target_val1 && val2 == target_val2);  // TRUE = target reached;
 }
 
+
+int sign(int x) {
+  if(x > 0)  return 1;
+  return -1;
+}
 int next_test_mode_target() {
     static int arrived = 0;  // 0 - cross, 1 - V or H
 
+#ifdef DEBUG_DAC
+    sprintf(outdbg, "next test target\n  in: %d %d   %d %d", 
+                    val1, target_val1, val2, target_val2);
+    Serial.println(outdbg);
+#endif
+
     if(abs(target_val1) != 32767)
-        target_val1 = -abs(target_val1)/target_val1 * 32767;
+        target_val1 = -sign(target_val1) * 32767;
     if(abs(target_val2) != 32767)
-        target_val1 = -abs(target_val2)/target_val2 * 32767;
+        target_val2 = -sign(target_val2) * 32767;
         
     if(arrived) {
         // arrived V or H, next go across
@@ -134,6 +159,10 @@ int next_test_mode_target() {
             target_val1 = -target_val1;
         arrived = 1;
     }
+#ifdef DEBUG_DAC
+    sprintf(outdbg, " out: %d %d   %d %d", val1, target_val1, val2, target_val2);
+    Serial.println(outdbg);
+#endif
 }
 
 // End of DAC functions
@@ -144,19 +173,369 @@ int next_test_mode_target() {
 
 
 
-void loop() {
-  //delay(1);
 
+
+
+
+
+// DIAL WHEEL ROUTINES
+
+long debounceDelay = 10;
+
+// debounces input pin
+int readDialInputPin()
+{
+  static int buttonState;
+  static int lastButtonState = LOW;
+  static long lastDebounceTime = 0;
+  int reading = digitalRead(dialInputPin);
+  
+  if (reading != lastButtonState) 
+    lastDebounceTime = millis();
+  if ((millis() - lastDebounceTime) > debounceDelay) 
+    if (reading != buttonState) 
+      buttonState = reading;
+  lastButtonState = reading;
+  return buttonState;
+}
+
+
+int lastDialPinState = readDialInputPin();
+long lastLowStatePeriod;
+long lastHighStatePeriod;
+
+// detects and measures input pin state changes
+int dialPinStateChanged()
+{
+  static long startLowStateTime;
+  static long endLowStateTime;
+  static long startHighStateTime;
+  static long endHighStateTime;
+
+  int change = false;
+  
+  if(readDialInputPin() == LOW && lastDialPinState == HIGH)
+  {
+    // Serial.println("H/L");
+    // falling edge
+    endHighStateTime = millis();
+    startLowStateTime = endHighStateTime;
+    lastHighStatePeriod = endHighStateTime - startHighStateTime;
+    lastDialPinState = LOW;
+    change = true;
+  }
+  else
+  if(readDialInputPin() == HIGH && lastDialPinState == LOW)
+  {
+    // Serial.println("L/H");
+    // rising edge
+    endLowStateTime = millis();
+    startHighStateTime = endLowStateTime;
+    lastLowStatePeriod = endLowStateTime - startLowStateTime;
+    lastDialPinState = HIGH;
+    change = true;
+  }
+  return change;
+}
+
+
+
+#define MAX_DIAL_NUMBER_LEN   20
+#define RESET_DELAY_DIAL     100
+char dialDigits[MAX_DIAL_NUMBER_LEN+1];  // dialed digits buffer
+int  dialDigitsNum = -2;    // dialed digits count
+char sound[200];  // buffer of digits: pitch, length
+
+void resetDialDigits() {
+  dialDigitsNum = -1;
+  memset(dialDigits, 0, sizeof(dialDigits));
+}
+
+// Detects long unchanged state periods.
+// Doeas not wait. Returns instantly. Must be run in endless loop.
+// returns 
+//      0 - picked up; no digits dialed yet
+//    > 0 - number of digits dialed
+//    < 0 - hanged up; negative number of dialed digits decreased by 1
+// dialed number is stored in global dialDigits[]
+int dialDigitDetected()
+{
+  static int  dialPhase;             // 0-idle (hanged up); 1-picked up; 2-pulsing
+  //static long resetDelayDial = 100;  // pulse length threshold (ms)
+  
+  static long lastDialPinChangeTime = 0;
+  static int  dialCounter; // keeps current digit value during pulsing phase
+  
+  // this part checks if dial eom condition hapened
+  if(millis() - lastDialPinChangeTime > RESET_DELAY_DIAL)
+  {
+    switch(dialPhase)
+    {
+      case 0:  // idle
+              if(lastDialPinState == LOW)
+              {
+                // Serial.println("idle + H/L => earphone picked up");
+                // idle + H/L => earphone picked up
+                // dialing did not start yet
+                //dialDigitsNum = -1;  // clear digits counter
+                resetDialDigits();
+                dialPhase = 1;       // set "earphone up" phase
+              }
+              break;
+      case 1: // earphone up
+              if(lastDialPinState == HIGH) {
+                // Serial.println("long H while earphone up - hanged up");
+                dialPhase = 0;       // get idle
+                dialDigitsNum = -dialDigitsNum - 3;
+              }
+              break;
+      case 2: // pulsing
+              if(lastDialPinState == HIGH) {
+                // Serial.println("long H after pulsing - apparently hanged up while dialing");
+                dialCounter = 0;     // reset erroneous digit
+                dialPhase = 0;       // get idle
+              }
+              else {
+                // Serial.println("long low after pulsing - finished dialing a digit");
+                dialPhase = 1;       // set the earphone up phase
+                dialDigitsNum++;     // save the next digit
+                if(dialDigitsNum == MAX_DIAL_NUMBER_LEN)
+                  dialDigitsNum = 0; // reset dialed number if length exceeds buffer
+                dialDigits[dialDigitsNum] = '0' + (dialCounter < 10 ? dialCounter : 0);
+                dialDigits[dialDigitsNum+1] = '\0';
+              }
+              break;
+    }
+    lastDialPinChangeTime = millis();
+    return dialDigitsNum + 1;
+  }
+  
+  if(dialPinStateChanged())
+  {
+    lastDialPinChangeTime = millis(); // for the long state detection
+    switch(dialPhase)
+    {
+      case 1:  // earphone up phase
+              if(lastDialPinState == HIGH) { 
+                                     // L/H => first pulse arriving or hanging up
+                dialCounter = 0;     // reset pulse counter
+              }
+              else {                 // earphone up + H/L => first pulse in series arrived
+                dialPhase = 2;       // set pulsing phase
+                dialCounter++;
+              }
+              break;
+      case 2:  // pulsing phase
+              if(lastDialPinState == LOW)
+                dialCounter++;  // pulsing + H/L => next pulse in series arrived
+              break;
+    }
+  }
+  return dialDigitsNum + 1;
+}
+
+
+
+
+void beep(char *input) {
+  static int curNoteIdx = 0;
+  static int curNoteVal;
+  static int phase = 0;
+  static char freq[100];
+  static char dur[100];
+  static int curNoteDur;
+  static int soundlen;
+  static char outlevel;
+  int inplen = strlen(input);
+
+  inplen = inplen > 200 ? 200/2 : inplen/2;
+  // setup sound
+  if(inplen > 0) {
+    // read input tones 
+    for(int ff=0; ff<inplen; ff++) {
+      freq[ff] = input[ff*2];
+      dur[ff] = input[ff*2+1] * 2;
+#ifdef DEBUG_DIAL
+    Serial.print((int)freq[ff]);
+    Serial.print(",");
+    Serial.print((int)dur[ff]);
+    Serial.print(" ");
+#endif
+    }
+    soundlen = inplen;
+    curNoteIdx = 0;
+    curNoteVal = freq[curNoteIdx];
+    curNoteDur = dur[curNoteIdx] * 50;
+    phase = 0;
+  }
+  
+  // play sound
+  if(curNoteIdx < soundlen) {
+  //if(1) {
+      phase++;
+      switch(curNoteVal) {
+        case 10:
+            break;
+        case 11:
+            break;
+        default:
+            if(phase % curNoteVal == 0) {
+                outlevel = 1 - outlevel;
+                digitalWrite(soundOutputPin, outlevel);
+             }
+      }
+      phase %= curNoteDur;
+      if(phase == 0) {
+          curNoteIdx++;
+          curNoteVal = freq[curNoteIdx];
+          curNoteDur = dur[curNoteIdx] * 50;
+      }
+  }
+  else
+      digitalWrite(soundOutputPin, LOW);
+}
+
+void setSoundTxt(char *txt) {
+    int len = strlen(txt) / 2;
+    memset(sound, 0, sizeof(sound));
+    for(int ff=0; ff<len; ff++) {
+        sound[ff*2] = txt[ff*2] + 3 - '0';
+        sound[ff*2+1] = txt[ff*2+1] - '0';
+    }
+    beep(sound);
+}
+void setSoundTxtSteady(char *txt, char notelen) {
+    int txtlen = strlen(txt);
+    memset(sound, 0, sizeof(sound));
+    for(int ff=0; ff<txtlen; ff++) {
+        sound[ff*2] = txt[ff] + 3 - '0';
+        sound[ff*2+1] = notelen;
+    }
+    beep(sound);
+}
+
+#define TONE_NONE    0
+#define TONE_READY   1
+#define TONE_FREE    2
+#define TONE_BUSY    3
+
+void setTone(int mode) {
+  static int duty;
+  static int full_period;
+  static int phase;
+  static int phase_step;
+  
+  if(phase == 0)
+      digitalWrite(tonePin, HIGH);  // turn tone on
+  if(phase == duty)
+      digitalWrite(tonePin, LOW);   // turn tone off
+  phase += phase_step;
+  phase %= full_period;
+  
+  switch(mode) {
+    case TONE_NONE:
+      duty = 1;
+      full_period = 3;
+      phase = duty;
+      phase_step = 0;
+      break;
+    case TONE_READY:
+      duty = 1;
+      full_period = 3;
+      phase = 0;
+      phase_step = 0;
+      break;
+    case TONE_FREE:
+      duty = 2000;
+      full_period = 8000;
+      phase = 0;
+      phase_step = 1;
+      break;
+    case TONE_BUSY:
+      duty = 800;
+      full_period = 1600;
+      phase = 0;
+      phase_step = 1;
+      break;
+  }
+  
+}
+
+// END OF DIAL WHEEL ROUTINES
+
+
+
+
+
+
+
+
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  pinMode(led, OUTPUT); 
+
+  // dial pins
+  pinMode(dialInputPin, INPUT);
+  pinMode(soundOutputPin, OUTPUT);
+  pinMode(tonePin, OUTPUT);
+
+  // Pins for TDA1543 DAC
+  pinMode(ws, OUTPUT);
+  pinMode(bck, OUTPUT);
+  pinMode(data, OUTPUT);
+  randomSeed(1809);
+
+  // plotter pin
+  pinMode(mode, INPUT);  // 0 - work, 1 - test
+  pinMode(powerOff, OUTPUT);  // 0 - plotter power ON
+  set_target(0, 0);
+  
+  Serial.begin(9600); 
+}
+
+
+
+
+
+
+
+  static char test_mode = 0;
+  void setTestMode (int mode) {
+    if(mode == 1)
+      test_mode = digitalRead(mode);     // turn on
+    else if(mode == 0)
+      test_mode = 1 - digitalRead(mode); // turn off
+    else
+      test_mode = 1 - test_mode;         // toggle
+  };
+
+void loop() {
+  // dac variables
+  static int ii;
+  
+  // dial wheel variables
+  int dc = 0;
+  static int last_dc = 0;
+  static char sound[200];  // input buffer for beep()
+  
+  
   // DAC controlling section
   // send data to DAC
   send_data(val1, val2);
+  
+  if(story_length != 0) {
   if(ii >= 5000) {
     ii = 0;
-    if(digitalRead(mode) == HIGH) {
+    if(digitalRead(mode) == test_mode) {
         next_test_mode_target();
     } else {
         target_val1 = random(-32768, 32767);
         target_val2 = random(-32768, 32767);
+        
+        if(story_length > 0)
+            story_length--;
     }
     set_target(target_val1, target_val2);
   }
@@ -168,21 +547,99 @@ void loop() {
     if(ii == 0)
       ii = 1; // bump it to make it counting
   }
+    digitalWrite(powerOff, 0);  // Turn plotter ON
+  } // if story_length
+  else
+    digitalWrite(powerOff, 1);  // turn plotter OFF
+
   // End of DAC controlling section
   
-  
-  
-  
 
-/*
-    Serial.print((long)target_val1);
-    Serial.print("  ");
-    Serial.print((long)dval1);
-    Serial.print("  ");
-    Serial.print((long)dstep1);
-    Serial.print("  ");
-    Serial.print(ii);
-    Serial.print("  ");
-    Serial.println(val1);
-*/
+
+  // Dial wheel controlling section
+  
+  digitalWrite(led, digitalRead(dialInputPin));   // turn the LED on (HIGH is the voltage level)
+     
+  dc = dialDigitDetected();
+  beep("");     // manage tones generated by uP
+  setTone(-1);  // manage tones generated by external device
+  
+  if(dc != last_dc)
+  {
+    last_dc = dc;
+    if(dc == 0) {
+#ifdef DEBUG_DIAL
+      Serial.print(dc);
+      Serial.println(": Picked up.");
+#endif
+      setTone(TONE_READY);
+    }
+    if(dc < 0) {
+#ifdef DEBUG_DIAL
+      Serial.print(dc);
+      Serial.println(": Hanged up.");      
+#endif
+      setTone(TONE_NONE);
+    }
+    if(dc > 0)
+    { 
+      // Put number driven commands here
+      
+      if(strncmp(dialDigits, "7173017", 7) == 0) {
+          setTestMode(-1);  // toggle test mode
+#ifdef DEBUG_STORY
+          Serial.print("Test Mode ");
+          Serial.println(digitalRead(mode) == test_mode);
+#endif
+          // resetDialDigits(); 
+          story_length = -1;
+          setSoundTxt("01110141");
+      }
+      else
+      if(dialDigits[0] == '9' && dc == 7) {
+           // set new story (seed)
+           int seed;
+           sscanf(&dialDigits[1], "%d", &seed);
+           randomSeed(seed);
+           story_length = random(20) + 3;
+           setSoundTxtSteady(dialDigits, 2);
+           setTestMode(0);
+           // resetDialDigits(); 
+#ifdef DEBUG_STORY
+    sprintf(outdbg, "seed %d, length %d", seed, story_length);
+    Serial.println(outdbg);
+#endif
+      }
+      else {
+
+      if(dc > 12)
+          setTone(TONE_BUSY);
+      else
+      if(dc > 10)
+          setTone(TONE_FREE);
+      else
+          setTone(TONE_NONE);
+
+
+        /*
+        memset(sound, 0, sizeof(sound));
+        for(int ff=0; ff<dc; ff++) {
+            sound[ff*2] = dialDigits[ff] + 3 - '0';
+            sound[ff*2+1] = 2;
+        }
+        beep(sound);
+        */
+#ifdef DEBUG_DIAL
+        Serial.print("Dialed Number[");
+        Serial.print(dc);
+        Serial.print("]: ");
+        Serial.print(dialDigits);
+        Serial.println("");
+#endif
+      }
+    }
+  }
+
+  // End of dial wheel controlling section
+  
 }
