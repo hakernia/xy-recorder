b83e4624541ac85bf4382f13e0ce83ccbdae60da
Piotr Karpiewski <karpiewski.git@gmail.com>
2017-11-22 14:13:19 +0100

    Each channel gets different value. Two modes of operation.

--- xy_recorder.ino
+++ xy_recorder.ino
@@ -1,8 +1,8 @@
 /*
   xy_recorder.ino
-  
+
   Sends data to TDA1543 double DAC
-  
+
  */
  
 // DAC PINs
@@ -10,8 +10,11 @@ int ws = 2;
 int bck = 3;
 int data = 4;
 
+int mode = 12;
+
 int ii = 0;
-short val = 0xFF;
+short val1 = 0xFF;
+short val2 = 0xFF;
 
 // the setup routine runs once when you press reset:
 void setup() {                
@@ -19,13 +22,16 @@ void setup() {
   pinMode(ws, OUTPUT);     
   pinMode(bck, OUTPUT);     
   pinMode(data, OUTPUT);     
+
+  pinMode(mode, INPUT);
+
 }
 
 void send_data(short value_l, short value_r) {
   int ff;
   int ch;
   short value;
-  
+
   for(ch=0; ch < 2; ch++) {
     if(ch==0)
       value = value_l;
@@ -48,14 +54,26 @@ void send_data(short value_l, short value_r) {
 void loop() {
   //delay(1);
   
-  send_data(val, val);
-  if(val >=32600)
-  {
+  send_data(val1, val2);
+  if(ii >= 10000) {
     ii = 0;
-    val = 0x8000;
   }
   else {
     ii++;
-    val += 100;
+  }
+ 
+  if(digitalRead(mode) == HIGH) {
+    if(ii == 0) {
+      val1 = random(65535)-32767;
+      val2 = random(65535)-32767;
+    }
+  }
+  else {  
+    if(val2 >=32600) {
+      val2 = 0x8000;
+    }
+    else {
+      val2 += 10;
+    }
   }
 }
